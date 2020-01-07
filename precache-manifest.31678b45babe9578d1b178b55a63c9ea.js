aa58c50e49f0ab9bf5c997675a5ae3a1d296b5e2
Toshi-Tabata <48075791+Toshi-Tabata@users.noreply.github.com>
2020-01-07 19:45:44 +1100

    push

---
+++ precache-manifest.31678b45babe9578d1b178b55a63c9ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476611a234c3a4353df2b607d31a25c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b6bb2d4cab495321718",
+    "url": "/static/css/main.3847e146.chunk.css"
+  },
+  {
+    "revision": "f8400a572fce03bc42d7",
+    "url": "/static/js/2.a400f49c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a400f49c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b6bb2d4cab495321718",
+    "url": "/static/js/main.0a563cb6.chunk.js"
+  },
+  {
+    "revision": "833afa376532221ce86f",
+    "url": "/static/js/runtime-main.a2e0dc0d.js"
+  }
+]);
